d95605f9a3dfefdb0e3458ba3400679cc0781bb6
A B M Asadullah <abmasadullah@gmail.com>
2021-07-15 01:24:07 +0600

    Complete the project and try to fix linters

--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
-import _ from 'lodash';
 import './style.css';
 
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-  
-    return element;
+const lst = [
+  {
+    index: 1,
+    description: 'Complete tast',
+    completed: true,
+  },
+  {
+    index: 2,
+    description: 'Go for walk',
+    completed: false,
+  },
+];
+
+const selectList = document.getElementById('listed');
+
+function viewList(){
+  for (let i = 1; i <= lst.length; i += 1) {
+    for (let j = 0; j < lst.length; j += 1) {
+      if (lst[j].index === i) {
+        const toDoLi = document.createElement('li');
+        toDoLi.className = 'item';
+        toDoLi.innerHTML = `
+                  <div class="check-div">
+                  <input id="input-${lst[j].index}" type="checkbox"/><label for="input-${lst[j].index}" >${lst[j].description}</label></div>
+                  <button><i class="fas fa-ellipsis-v dot-icon"></i></button>`;
+        selectList.appendChild(toDoLi);
+        selectList.appendChild(document.createElement('hr'));
+      }
+    }
   }
-  
-  document.body.appendChild(component());
+}
+
+document.addEventListener('DOMContentLoaded', viewList);
