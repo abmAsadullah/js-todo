8c2df1be81dea871048ed02565ae72c41517a9e7
A B M Asadullah <abmasadullah@gmail.com>
2021-07-16 04:18:06 +0600

    Make dropdown

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { Drag } from "./drop.js";
 import './style.css';
 
 const lst = [
@@ -11,6 +12,16 @@ const lst = [
     description: 'Go for walk',
     completed: false,
   },
+  {
+    index: 3,
+    description: 'Lodry',
+    completed: true,
+  },
+  {
+    index: 4,
+    description: 'Go for daily exercise',
+    completed: false,
+  },
 ];
 
 const selectList = document.getElementById('listed');
@@ -20,16 +31,31 @@ function viewList() {
     for (let j = 0; j < lst.length; j += 1) {
       if (lst[j].index === i) {
         const toDoLi = document.createElement('li');
-        toDoLi.className = 'item';
+        toDoLi.className = `item ${lst[j].index}`;
         toDoLi.innerHTML = `
                   <div class="check-div">
-                  <input id="input-${lst[j].index}" type="checkbox"/><label for="input-${lst[j].index}" >${lst[j].description}</label></div>
-                  <button><i class="fas fa-ellipsis-v dot-icon"></i></button>`;
+                  <input id="input-${lst[j].index}" type="checkbox"/><textarea name="description"">${lst[j].description}</textarea></div>
+                  <button draggable="true"><i class="fas fa-ellipsis-v dot-icon"></i></button>`;
         selectList.appendChild(toDoLi);
-        selectList.appendChild(document.createElement('hr'));
+        // selectList.appendChild(document.createElement('hr'));
       }
     }
   }
 }
 
 document.addEventListener('DOMContentLoaded', viewList);
+
+
+function loadLiEvents() {
+  const liElements = document.querySelectorAll(".item");
+  // console.log(liElements);
+  for (let i = 0; i < liElements.length; i++) {
+    const drag = new Drag();
+    liElements[i].addEventListener("dragstart", drag.dragStart);
+    liElements[i].addEventListener("dragover", drag.dragOver);
+    liElements[i].addEventListener("dragleave", drag.dragLeave);
+    liElements[i].addEventListener("drop", drag.drop);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", loadLiEvents);
